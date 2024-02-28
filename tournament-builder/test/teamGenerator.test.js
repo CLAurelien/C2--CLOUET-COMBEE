d5f20641d9419f7a94ab7ffdec1fbb1c1476b0ac
Aurélien <aurelien010603@gmail.com>
2024-02-28 16:38:46 +0100

    1st tu BDD type

---
+++ tournament-builder/test/teamGenerator.test.js
@@ -0,0 +1,18 @@
+import TeamGenerator from "../src/teamGenerator.js";
+
+const chai = import('chai');
+const expect = chai.expect;
+
+describe('TeamGenerator', () => {
+    it('nbGenerationTeam', async () => {
+        let player = ["hey1","hey2", "hey3"];
+        let teams = 3;
+        let result = 0;
+        let team = new TeamGenerator(player,teams);
+        team.generateTeams();
+        for(let i = 0; i<teams;i++){
+            result++;
+        }
+        (await chai).expect(result).equal(3)
+    });
+});
