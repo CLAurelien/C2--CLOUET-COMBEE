55ee899f952c595d7a5ac97406771859df3e5fda
Aurélien <aurelien010603@gmail.com>
2024-03-09 21:57:21 +0100

    2nd test TDD

--- tournament-builder/test/teamGenerator.test.js
+++ tournament-builder/test/teamGenerator.test.js
@@ -6,11 +6,11 @@ const chai = import('chai');
 describe('TeamGenerator', () => {
     it('nbGenerationTeamTypeBDD', async () => {
         let player = ["hey1","hey2", "hey3"];
-        let teams = 3;
+        let nbPerTeams = 3;
         let result = 0;
-        let team = new TeamGenerator(player,teams);
+        let team = new TeamGenerator(player,nbPerTeams);
         team.generateTeams();
-        for(let i = 0; i<teams;i++){
+        for(let i = 0; i<nbPerTeams;i++){
             result++;
         }
         (await chai).expect(result).equal(3)
@@ -24,8 +24,8 @@ describe('TeamGenerator', () => {
 describe('TournamentGenerator', () => {
     it('pouleNotNullBDD', async () => {
         let player = ["hey1","hey2", "hey3","hey4"];
-        let teams = 1;
-        let team = new TeamGenerator(player,teams);
+        let nbPerTeams = 1;
+        let team = new TeamGenerator(player,nbPerTeams);
         team.generateTeams();
         let getTeam = team.getTeams();
         let tournament = new TournamentGenerator(getTeam);
@@ -33,7 +33,17 @@ describe('TournamentGenerator', () => {
         (await chai).expect(tournament.poules).length(!0)
     });
 
-    it('TDD', () => {
-        
+    it('nbTeams>4TDD', async () => {
+        let player = [];
+        let teams = 1;
+        for (;teams<4;teams++){
+            player.push('hey'+teams.toString());
+            let team = new TeamGenerator(player, teams);
+            team.generateTeams();
+            let getTeam = team.getTeams();
+            let tournament = new TournamentGenerator(getTeam);
+            tournament.generateTournament();
+            (await chai).expect(tournament.nbPoulesExeption())
+        }
     });
 });
